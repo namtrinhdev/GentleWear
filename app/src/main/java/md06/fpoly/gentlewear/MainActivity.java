f208f9a68a4da513a7923be38a6559fffb3e12a2
namtrinh2k <namtd20112000@gmail.com>
2024-01-22 20:29:31 +0700

    Initial commit

---
+++ app/src/main/java/md06/fpoly/gentlewear/MainActivity.java
@@ -0,0 +1,14 @@
+package md06.fpoly.gentlewear;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
