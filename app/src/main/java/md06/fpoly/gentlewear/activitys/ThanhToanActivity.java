a9d5ccdd988e2b1be54906320f3e8d03a6849887
namtrinh <namtd20112000@gmail.com>
2024-03-20 22:13:03 +0700

    giao dien thanh toan

---
+++ app/src/main/java/md06/fpoly/gentlewear/activitys/ThanhToanActivity.java
@@ -0,0 +1,16 @@
+package md06.fpoly.gentlewear.activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import md06.fpoly.gentlewear.R;
+
+public class ThanhToanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thanh_toan);
+    }
+}
