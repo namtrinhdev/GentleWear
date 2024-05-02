d9abaa691beeb3c9740b67d6c75cf2b9eb5b69e1
nguyenvanquoc03 <117958456+nguyenvanquoc03@users.noreply.github.com>
2024-05-02 20:42:52 +0700

    fix

--- app/src/main/java/md06/fpoly/gentlewear/classs/APIClass.java
+++ app/src/main/java/md06/fpoly/gentlewear/classs/APIClass.java
@@ -1,6 +1,6 @@
 package md06.fpoly.gentlewear.classs;
 
 public class APIClass {
-    static final String LOCALHOST = "192.168.192.128";
+    static final String LOCALHOST = "10.24.1.228";
     public static final String URL ="http://"+LOCALHOST+":3000/";
 }
